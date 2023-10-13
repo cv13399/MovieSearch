451448a4f562f4cf69e3d9aaddf9f74adafe89b0
Chase Lee <chase@study.cat>
2023-10-14 02:16:54 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import { React, useEffect, useState } from 'react';
 import './App.css';
+import searchIcon from './search.svg';
+// import staticMovies from './staticMovieData';
+import MovieCard from './MovieCard.jsx';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api_url = 'http://www.omdbapi.com?apikey=b5b87478';
+
+const App = () => {
+    // let sampleMovie = staticMovies[0];
+
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+
+    // get movie data from API
+    const fetchMovies = async(title) =>{
+        const response = await fetch(`${api_url}&s=${title}`);
+        const data = await response.json();
+        console.log("DATA: ", data);
+
+        if (data.Response === "True") {
+            setMovies(data.Search);
+        }else{
+            alert("Can't fetch movie data");
+            console.log(data);
+        }
+    };
+
+    // with [] as second param, this will only run onReload
+    useEffect(() => {
+        fetchMovies("One Day");
+    }, []);
+
+    return (
+        <div className='app'>
+            <button className='menu'>
+                menu
+            </button>
+
+            <h1>MovieLand</h1>
+
+            <div className='search'>
+                <input
+                    placeholder='Search for movies'
+                    value={searchTerm}
+                    onChange={(event) => {
+                        setSearchTerm(event.target.value)
+                    }}
+                />
+                <img
+                    src={searchIcon}
+                    alt="search"
+                    onClick={() => fetchMovies(searchTerm)}
+                />
+            </div>
+            {movies?.length > 0 ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie} />
+                        ))}
+                    </div>
+                ):(
+                    <div className='empty'>
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
-export default App;
+export default App
---
+++ src/staticMovieData.js
@@ -0,0 +1,75 @@
+const staticMovies =
+[
+    {
+        "Title": "One Day",
+        "Year": "2011",
+        "imdbID": "tt1563738",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTQ3NTg2MDI3NF5BMl5BanBnXkFtZTcwMjc5MTA1NA@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Fine Day",
+        "Year": "1996",
+        "imdbID": "tt0117247",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BZjlkODI1ZDYtOTAwMy00ZTMxLThhMzgtMmE4MzFiNzQyYmRmXkEyXkFqcGdeQXVyMTUzMDUzNTI3._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Day at a Time",
+        "Year": "2017–2020",
+        "imdbID": "tt5339440",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BOGIwNzA2OWItMDdjYS00YWRhLWE3ODgtN2NhNzIzZmY3ZjZhXkEyXkFqcGdeQXVyODM3ODkyMDk@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Day in September",
+        "Year": "1999",
+        "imdbID": "tt0230591",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYWI0MGJkOGEtMzViNS00YzRmLTllMGQtZmJhZjk0Mzg3YzhhXkEyXkFqcGdeQXVyMjA0MzYwMDY@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Day at a Time",
+        "Year": "1975–1984",
+        "imdbID": "tt0072554",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTQ1ODQzODg2NF5BMl5BanBnXkFtZTcwMjI4NjM0MQ@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "9/11: One Day in America",
+        "Year": "2021",
+        "imdbID": "tt14734548",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNzFlYmMyNGEtYzRlMS00MTc5LWFiNDItNmJlODNiMTAwMGNmXkEyXkFqcGdeQXVyNTM0NTU5Mg@@._V1_SX300.jpg"
+    },
+    {
+        "Title": "Earth: One Amazing Day",
+        "Year": "2017",
+        "imdbID": "tt6238896",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYmZhYzY2NjItZjFkMy00ZDcyLTk5NTYtNDgwOGQ1YjdiZGZjXkEyXkFqcGdeQXVyMjQzOTEyMzY@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Ordinary Day",
+        "Year": "2021–",
+        "imdbID": "tt14170016",
+        "Type": "series",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMWI5YWY5OTEtZDFjMC00YWU2LTkxYzMtN2JjMGQ0ODQ1NDliXkEyXkFqcGdeQXVyNzkxOTQzMzE@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Day: Justice Delivered",
+        "Year": "2019",
+        "imdbID": "tt8130558",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4YjBiY2YtMzNlZC00ZjBkLWJlMWItMzEyOTdhNWVhMzkyXkEyXkFqcGdeQXVyMzQwOTA2MTY@._V1_SX300.jpg"
+    },
+    {
+        "Title": "One Day",
+        "Year": "2016",
+        "imdbID": "tt6168298",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BYmYwZjQwNmMtMmE0Mi00OTZmLWFhMGQtMTI0ZjJlOTI0MWY3XkEyXkFqcGdeQXVyNzI1NzMxNzM@._V1_SX300.jpg"
+    }
+]
+
+export default staticMovies;
