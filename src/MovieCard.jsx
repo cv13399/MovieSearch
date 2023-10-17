81ed40aa152ac829587aff3af6e266c54e104e67
Chase Lee <chase@study.cat>
2023-10-17 13:10:46 +0800

    added link to go to imdb website

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,12 +1,23 @@
+
 const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+    const onCardClicked = (id) =>{
+      window.open("https://www.imdb.com/title/"+ `${id}`)
+    }
+
     return (
-      <div className="movie" key={imdbID}>
+      <div className="movie" key={imdbID}
+        onClick={() => onCardClicked(imdbID)}
+        role="button"
+      >
         <div>
           <p>{Year}</p>
         </div>
   
         <div>
-          <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+          <img 
+            src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} 
+            alt={Title}
+          />
         </div>
   
         <div>
@@ -16,5 +27,7 @@ const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
       </div>
     );
 }
-  
+
+
+
 export default MovieCard;
